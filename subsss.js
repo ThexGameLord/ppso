eea11bd78d6ad0ebaffde564541658788663ff80
ThexGameLord <69915399+ThexGameLord@users.noreply.github.com>
2023-08-28 21:49:31 +0100

    Create subsss.js

---
+++ subsss.js
@@ -0,0 +1,29 @@
+<script>
+document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the form from submitting traditionally
+
+  // Get form data
+  var formData = new FormData(event.target);
+
+  // Convert form data to URL-encoded format
+  var encodedFormData = new URLSearchParams(formData).toString();
+
+  // Send a POST request to the subscription endpoint
+  fetch('https://reviiiveme.thexgamelord.repl.co/subscribe', {
+    method: 'POST',
+    body: encodedFormData,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  .then(response => response.text())
+  .then(message => {
+    // Update the message element with the response message
+    //document.getElementById('message').innerText = message;
+    alert(message)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
+</script>
